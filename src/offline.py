5ec216c9bd26c0e726bda96bd4c662f6e9b22da6
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-04-07 15:58:31 -0400

    Add files via upload

--- src/offline.py
+++ src/offline.py
@@ -1,18 +1,17 @@
 import pandas as pd
-from river import stream
-from river import forest
-from river import metrics
+from xgboost.sklearn import XGBRegressor
+from sklearn.metrics import mean_squared_error
 
 
-def streaming(df, outputs, test_frac):
-    model = Streaming()
+def batching(df, outputs, test_frac):
+    model = Batching()
     model.train(df, outputs, test_frac)
     model.predict()
 
     return model
 
 
-class Streaming:
+class Batching:
     def train(self, df, outputs, test_frac):
         self.data = df.copy().sample(frac=1, random_state=0).reset_index(drop=True)  # shuffle the data
         self.outputs = outputs
@@ -25,10 +24,17 @@ class Streaming:
             X = train.copy().drop(columns=self.outputs)
             Y = train.copy()[[out]]
 
-            model = forest.ARFRegressor(n_models=10, seed=42)
-
-            for x, y in stream.iter_pandas(X, Y):
-                model = model.learn_one(x, y[out])
+            model = XGBRegressor(
+                booster="gbtree",
+                n_estimators=100, 
+                learning_rate=0.1,
+                max_depth=7,
+                min_child_weight=1,
+                colsample_bytree=0.8,
+                subsample=0.8,
+                random_state=42,
+            )
+            model.fit(X, Y)
 
             self.model[out] = model
 
@@ -43,23 +49,20 @@ class Streaming:
             Y = test.copy()[[out]]
 
             model = self.model[out]
-            metric = metrics.RMSE()
-            predictions = pd.DataFrame()
+            y_pred = model.predict(X)
+            y_true = Y.to_numpy().ravel()
 
-            for x, y in stream.iter_pandas(X, Y):
-                y_pred = model.predict_one(x)
-                metric = metric.update(y[out], y_pred)
-                model = model.learn_one(x, y[out])
+            metric = mean_squared_error(
+                y_true=y_true, 
+                y_pred=y_pred, 
+                squared=False,
+            )
+            metric = f"RMSE: {round(metric, 6)}"
 
-                pred = pd.DataFrame({
-                    "Actual": [y[out]],
-                    "Predicted": [y_pred],
-                })
-                predictions = pd.concat([
-                    predictions, 
-                    pred,
-                ], axis="index").reset_index(drop=True)
+            predictions = pd.DataFrame({
+                "Actual": y_true,
+                "Predicted": y_pred,
+            })
 
-            self.model[out] = model
             self.metric[out] = metric
             self.predictions[out] = predictions
